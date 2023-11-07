59877404d105f94b1b60937333f80248acbd75aa
Arul6851 <12304.arul@gmail.com>
2023-11-08 00:19:27 +0530

    ex5 completed

--- ex3/app.js
+++ ex3/app.js
@@ -1,35 +1,41 @@
 const credentials = [
   {
-    username: "admin@mail.com",
+    email: "admin@mail.com",
     password: "1234",
+    username: "admin",
   },
   {
-    username: "user@mail.com",
+    email: "arul@mail.com",
     password: "5678",
+    username: "arul",
   },
 ];
 
 const validate = (event) => {
   event.preventDefault();
 
-  var i_email = document.getElementById("email").value;
+  var i_name = document.getElementById("name").value;
   var i_password = document.getElementById("password").value;
   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   let isValid = false;
 
   credentials.forEach((element) => {
-    if (element.username === i_email && element.password === i_password) {
+    if (
+      (element.username === i_name || element.email === i_name) &&
+      element.password === i_password
+    ) {
       isValid = true;
     }
   });
 
-  if (i_email === "" || i_password === "") {
+  if (i_name === "" || i_password === "") {
     alert("Fields cannot be null");
-  } else if (!i_email.match(emailPattern)) {
-    alert("Enter a valid email");
   } else if (!isValid) {
     alert("Enter the right credentials");
+  } else if (!i_name.match(emailPattern)) {
+    alert("Enter a valid email");
   } else {
-    window.location.href = "signup.html";
+    // window.location.href = "signup.html";
+    alert("Login Successful!");
   }
 };
---
+++ ex5a/WEB-INF/classes/LoginServlet.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private List<Credentials> credentials = new ArrayList<>();
+
+    public LoginServlet() {
+        credentials.add(new Credentials("admin", "1234", "admin@mail.com"));
+        credentials.add(new Credentials("arul", "5678", "arul@mail.com"));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String i_name = request.getParameter("name");
+        String i_password = request.getParameter("pass");
+        String emailPattern = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
+        boolean isValid = false;
+
+        for (Credentials credential : credentials) {
+            if ((credential.getUsername().equals(i_name) || credential.getEmail().equals(i_name))
+                    && credential.getPassword().equals(i_password)) {
+                isValid = true;
+                break;
+            }
+        }
+
+        if (i_name.isEmpty() || i_password.isEmpty()) {
+            out.print("<html><body><p>Fields cannot be null</p></body></html>");
+        } else if (!isValid) {
+            out.print("<html><body><p>Enter the right credentials</p></body></html>");
+        } else if (!i_name.matches(emailPattern)) {
+            out.print("<html><body><p>Enter a valid email</p></body></html>");
+        } else {
+            out.print("<html><body><p>Login Successful!</p></body></html>");
+        }
+    }
+
+    public class Credentials {
+        private String username;
+        private String password;
+        private String email;
+
+        public Credentials(String username, String password, String email) {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+}
